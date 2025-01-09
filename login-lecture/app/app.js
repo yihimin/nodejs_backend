aa32c1bb20d4576d62fabc8ca93dd27fbba322cf
yihimin <yihimin01@gmail.com>
2025-01-09 11:20:39 +0900

    login.js

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -9,6 +9,7 @@ const home = require("./src/routes/home");
 // 앱 설정
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 // 라우터 등록
 app.use("/", home);
