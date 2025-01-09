761d087c94eb21244349421385bc9fa9e40f2a74
yihimin <yihimin01@gmail.com>
2025-01-09 11:11:30 +0900

    폴더구조최적화

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 
 // 라우터 가져오기
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 설정
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 // 라우터 등록
