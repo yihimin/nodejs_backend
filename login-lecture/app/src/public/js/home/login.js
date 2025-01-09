f3908e43b11bceeb702f736f55bfdf2249574b03
yihimin <yihimin01@gmail.com>
2025-01-09 11:38:32 +0900

    used DOM to relate login.js

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("llll");
+const id = document.querySelector("#id"),
+      pw = document.querySelector("#pw"),
+      loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+}
