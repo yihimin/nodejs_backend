1c5772da717096c4d8cb796f63e36361725327c4
yihimin <yihimin01@gmail.com>
2025-01-10 21:16:54 +0900

    회원가입 구현

--- login-lecture/app/src/models/UserStorage.js
+++ login-lecture/app/src/models/UserStorage.js
@@ -3,27 +3,37 @@
 const fs = require("fs").promises;
 
 class UserStorage {
-    static getUsers(...fields) {
-        //const users = this.#users;
+
+    static #getUsers(data, fields) {
+        const users = JSON.parse(data);
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
-        return newUsers;
+            return newUsers;
         }, {});
         return newUsers;
     }
 
-    static getUserInfo(id) {
+    static getUsers(...fields) {
         return fs
-        .readFile("./src/databases/users.json")
-        .then((data) => {
-            return this.#getUserInfo(data, id);
-        })
-        .catch(console.error);
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUsers(data, fields);
+            })
+            .catch(console.error);
     }
 
-    static #getUserInfo(data, id){
+    static getUserInfo(id) {
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
+    }
+
+    static #getUserInfo(data, id) {
         const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const userKeys = Object.keys(users);
@@ -34,12 +44,27 @@ class UserStorage {
         return userInfo;
     }
 
-    static save(userInfo) {
-        //const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.pw.push(userInfo.pw);
-        return { success: true };
+    static async save(userInfo) {
+        try {
+            const data = await fs.readFile("./src/databases/users.json", "utf-8");
+            const users = JSON.parse(data);
+
+            if (users.id.includes(userInfo.id)) {
+                throw "이미 존재하는 아이디입니다.";
+            }
+
+            // 기존 데이터에 새 유저 정보 추가
+            users.id.push(userInfo.id);
+            users.name.push(userInfo.name);
+            users.pw.push(userInfo.pw);
+
+            // 파일에 저장
+            await fs.writeFile("./src/databases/users.json", JSON.stringify(users, null, 2), "utf-8");
+            return { success: true };
+        } catch (error) {
+            console.error("Error saving user:", error);
+            throw error;
+        }
     }
 }
 
