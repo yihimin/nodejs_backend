8d9d2eef689ba9fc0c2222f39a7ce5f8d24fdca4
yihimin <yihimin01@gmail.com>
2025-01-20 12:08:12 +0900

    개발모드, 배포모드 로그

--- login-lecture/app/bin/www.js
+++ login-lecture/app/bin/www.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const app = require("../app");
+const logger = require("../src/config/logger");
 const PORT = process.env.PORT || 3000;
 
 // 서버 실행
 app.listen(PORT, () => {
-    console.log("Server On!");
+    logger.info(`${PORT}번 포트에서 서버 실행중입니다.`);
 });
--- login-lecture/app/src/config/logger.js
+++ login-lecture/app/src/config/logger.js
@@ -1,32 +1,35 @@
 const { lab } = require('color-convert');
 const { createLogger, transports, format } = require('winston');
-const { combine, timestamp, printf, label, colorize } = format;
+const { combine, timestamp, printf, label, colorize, simple } = format;
 
 const printFormat = printf(( { timestamp, label, level, message }) =>{
     return `${timestamp} [${ label }] ${level} : ${message}`;
 });
 
-const printLogFormat = combine(
+const printLogFormat = {
+    file: combine(
     label({ label: "백로그" }),
     timestamp({
         format: "YYYY-MM-DD HH:mm:ss",
     }),
     printFormat
-);
+    ),
+    console: combine(
+    colorize(),
+    simple()
+    ),
+};
 
 const opts = {
  file: new transports.File({
     filename: "access.log",
     dirname: "./logs",
     level: "info",
-    format: printLogFormat,
+    format: printLogFormat.file,
 }),
 console: new transports.Console({
     level: "info",
-    format: combine(
-        colorize(),
-        printLogFormat
-    ),
+    format: printLogFormat.console,
 }),
 };
 
