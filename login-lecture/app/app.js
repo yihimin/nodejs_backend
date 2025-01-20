5127a70ae3c95c41eee6b69c674b1ecd00613dcd
yihimin <yihimin01@gmail.com>
2025-01-20 11:02:36 +0900

    log

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -1,19 +1,31 @@
 "use strict";
 
+//모듈
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const accessLogStream = require("./src/config/log");
 
-// 라우터 가져오기
+const app = express();
+dotenv.config();
+
+// 라우팅
 const home = require("./src/routes/home");
 
 // 앱 설정
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+// 미들웨어
 app.use(express.static(`${__dirname}/src/public`));
-app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 
 // 라우터 등록
 app.use("/", home);
+
 
 module.exports = app;
